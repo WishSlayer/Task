6e6346fce468f1f6d4f5640d1af8baa50f08a723
WishSlayer <wishslayer@icloud.com>
2021-04-09 14:23:46 +0300

    dce

--- src/main/java/com/taskexam/dao/OrderLine.java
+++ src/main/java/com/taskexam/dao/OrderLine.java
@@ -2,11 +2,17 @@ package com.taskexam.dao;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import java.math.BigDecimal;
+import javax.persistence.JoinColumn;
 
 @Entity
 public class OrderLine extends BaseIdentity {
 
+    @JoinColumn(name = "orders_id")
+    private long order_id;
+
+    @JoinColumn(name = "goods_id")
+    private long goods_id;
+
     @Column(nullable = false)
-    private BigDecimal count;
+    private int count;
 }
